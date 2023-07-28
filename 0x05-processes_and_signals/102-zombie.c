1626ae1af14e37b45266c3ead032ce414fe7d531
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-29 01:02:06 +0300

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -6,40 +6,40 @@
 
 /**
  * infinite_while - function that is allways working
- * 
+ *
  * Return: int (0)
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 
 int main(void)
 {
-    pid_t pid;
-    int i;
+	pid_t pid;
+	int i;
 
-    for (i = 0 ; i < 5 ; i++)
-    {
-        pid = fork();
+	for (i = 0 ; i < 5 ; i++)
+	{
+		pid = fork();
 
-        if (pid > 0)
-        {
-            printf("Zombie process created, PID: %i/n", pid);
-            sleep(1);
-        }
-        else
-        {
-            exit(0);
-        }
-    }
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %i/n", pid);
+			sleep(1);
+		}
+		else
+		{
+			exit(0);
+		}
+	}
 
-    infinite_while();
+	infinite_while();
 
 	return (0);
 }
