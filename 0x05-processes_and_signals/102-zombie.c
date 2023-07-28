4d375b9308783a6f00b5d2c78a9aac17df7fe636
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-29 00:55:19 +0300

    Create 102-zombie.c

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/**
+ * infinite_while - function that is allways working
+ * 
+ * Return: int (0)
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+
+int main(void)
+{
+    pid_t pid;
+    int i;
+
+    for (i = 0 ; i < 5 ; i++)
+    {
+        pid = fork();
+
+        if (pid > 0)
+        {
+            printf("Zombie process created, PID: %i/n", pid);
+            sleep(1);
+        }
+        else
+        {
+            exit(0);
+        }
+    }
+
+    infinite_while();
+
+	return (0);
+}
+
