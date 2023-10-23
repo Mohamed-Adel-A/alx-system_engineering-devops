3df536be74218cafc3766e6cccdd43923b64e003
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-24 00:22:04 +0300

    Create 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+a Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress.
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+     user_id = argv[2]
+
+    api_url = "https://jsonplaceholder.typicode.com/users/"
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    todo_url = user_url + "/todos/"
+    
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    
+    todo_response = requests.get(todo_url)
+    todo_data = todo_response.json()
+    
+    completed_todo = [todo.get("title")
+                      for todo in data
+                      if todo.get("completed") == True]
+    
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_data.get("name"),
+                  len(completed_todo),
+                  len(todo_data)))
+    
+    for title in completed_todo:
+        print("\t {}".format(title))
