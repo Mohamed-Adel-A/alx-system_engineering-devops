270cdc36e20a02f6def6af271dea09f75a0f8298
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-08 01:46:02 +0200

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API and returns
+the number of subscribers (not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers for a given subreddit.
+    """
+    API_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(API_url, allow_redirects=False)
+    if response.status_code == 404:
+        return (0)
+    subs_data = response.json().get("data").get("subscribers")
+    return (subs_data)
