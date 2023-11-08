53d7b5541e2c44ba94264270fe749de2907bf5c0
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-08 02:07:56 +0200

    Create 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    """
+    API_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False)
+    if (response.status_code == 404):
+        print("None")
+        return
+    posts_data = response.json().get("data").get('children')
+    for post in posts_data:
+        print(post.get("data").get('title'))
