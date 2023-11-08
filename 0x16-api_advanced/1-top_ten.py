6e90963ba083f63f177eee5fbe4e118e886140ef
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-08 02:13:27 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,9 @@ def top_ten(subreddit):
     """
     """
     API_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    response = requests.get(API_url, allow_redirects=False)
+    response = requests.get(API_url,
+                            params = {"limit": 10},
+                            allow_redirects=False)
     if (response.status_code == 404):
         print("None")
         return
