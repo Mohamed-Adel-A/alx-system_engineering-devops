5e4f97ef72dcee9e7e05d7229bb8c018ccc5768c
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-08 03:06:37 +0200

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -28,6 +28,7 @@ def recurse(subreddit, hot_list=[], after=None):
         response = requests.get(API_url,
                                 headers={"User-Agent": "Mozilla/5.0"},
                                 allow_redirects=False)
+    print(response.status_code)
     if (response.status_code != 200):
         return None
     json_data = (response.json()).get("data")
