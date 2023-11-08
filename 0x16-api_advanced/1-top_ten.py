187eb93a81653e47493d079f0bde9a57d8f35083
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-08 02:17:56 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,5 +15,9 @@ def top_ten(subreddit):
         print("None")
         return
     posts_data = (response.json()).get("data").get('children')
+    i = 0
     for post in posts_data:
         print(post.get("data").get('title'))
+        i += 1
+        if i == 10:
+            break
