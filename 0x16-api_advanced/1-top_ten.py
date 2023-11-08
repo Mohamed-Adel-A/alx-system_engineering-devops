1c7921bc35b9ac89ed69e3fad30d3b4993edcea3
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-08 02:22:14 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,17 +8,22 @@ import requests
 
 def top_ten(subreddit):
     """
+    queries the Reddit API and
+    prints the titles of the first 10 hot posts
+    listed for a given subreddit.
     """
     API_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    response = requests.get(API_url, allow_redirects=False)
+    response = requests.get(API_url,
+                            headers={"User-Agent": "Mozilla/5.0"},
+                            allow_redirects=False)
     if (response.status_code == 404):
         print("None")
         return
     json_data = response.json()
-    posts_data = json_data.get("data").get('children')
+    posts_data = json_data.get("data").get("children")
     i = 0
     for post in posts_data:
-        print(post.get("data").get('title'))
+        print(post.get("data").get("title"))
         i += 1
         if i == 10:
             break
