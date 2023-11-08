de12de892edd6fe15bad114842f7ea63550fee3b
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-08 04:06:37 +0200

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -54,7 +54,7 @@ def count_words(subreddit, word_list, words_count_dict={}, after=None):
                     words_count_dict[word] = count
 
     if after:
-        return recurse(subreddit, word_list, after)
+        return count_words(subreddit, word_list, after)
 
     if not words_count_dict:
         print()
