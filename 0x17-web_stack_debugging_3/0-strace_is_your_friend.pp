8869fa568b4e7c780489db8d839c66855a3c9e54
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-09 00:18:57 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Apache is returning a 500 error fix it and then automate it using Puppet
 
 exec { 'fix Apache 500 error':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  provider => 'shell',
+  path    => '/usr/local/bin/:/bin/',
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
 }
