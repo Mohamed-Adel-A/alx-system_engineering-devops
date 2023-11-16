f7809d0a7ea0b5ca1562804d3bd5daaabd8c8661
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-16 05:09:21 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,3 +4,8 @@ exec {'restart nginx':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
+
+exec { 'nginx':
+  command => 'nginx -s stop && nginx',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
